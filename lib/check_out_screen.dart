c679e7d51bd712d90a386baf2df5831bc941559c
Chinedu Jeremiah <chinedujeremiah2002@gmail.com>
2022-07-30 14:51:37 +0100

    updated the ui

--- lib/check_out_screen.dart
+++ lib/check_out_screen.dart
@@ -11,6 +11,7 @@ class CheckOutScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
+        backgroundColor: Colors.white,
         title: Text(
           "Checkout",
           style: TextStyle(
@@ -18,18 +19,21 @@ class CheckOutScreen extends StatelessWidget {
             height: 1.29.h,
             letterSpacing: -0.41.w,
             fontWeight: FontWeight.w500,
+            color: Colors.black,
           ),
         ),
         leading: TextButton.icon(
           onPressed: () {},
           icon: const Icon(
             Icons.arrow_back_ios,
+            color: Colors.black,
           ),
           label: Text(
             "back",
             style: TextStyle(
               fontSize: 17.sp,
               height: 1.29.h,
+              color: Colors.black,
               letterSpacing: -0.41.w,
             ),
           ),
@@ -41,12 +45,12 @@ class CheckOutScreen extends StatelessWidget {
             Container(
               color: const Color(0xFFFFC300),
               width: double.maxFinite,
-              alignment: Alignment.center,
               height: 64.h,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Deliver to your door",
+                    "Deliver carefully to your door",
                     style: TextStyle(
                       fontSize: 13.sp,
                       fontWeight: FontWeight.w500,
@@ -64,7 +68,12 @@ class CheckOutScreen extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
+            Container(
+              margin: EdgeInsets.only(
+                left: 16.w,
+                top: 24.h,
+                bottom: 14.h,
+              ),
               child: Row(
                 children: [
                   ClipRRect(
@@ -76,7 +85,11 @@ class CheckOutScreen extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
+                  SizedBox(
+                    width: 24.w,
+                  ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         "Yellow bag edition",
@@ -86,14 +99,123 @@ class CheckOutScreen extends StatelessWidget {
                           fontWeight: FontWeight.w500,
                           letterSpacing: -0.41.w,
                         ),
-                      )
+                      ),
+                      Text(
+                        "100% Synthentic \n Leather",
+                        style: TextStyle(
+                          height: 1.33.h,
+                          fontSize: 15.sp,
+                          color: const Color(0xFF3C3C43),
+                        ),
+                      ),
                     ],
                   ),
                 ],
               ),
             ),
+            CheckoutCard(
+              children: [
+                Text(
+                  "Shipping Address",
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    fontWeight: FontWeight.w500,
+                    height: 1.29.h,
+                    letterSpacing: -0.41.w,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "234, Forest Street Lincolnton,NC 28092.",
+                      style: TextStyle(
+                        color: const Color(0xff3c3c43).withOpacity(0.60),
+                        fontSize: 15.sp,
+                        height: 1.33.h,
+                        letterSpacing: -0.24.w,
+                      ),
+                    ),
+                    const Icon(Icons.check),
+                  ],
+                ),
+                SizedBox(
+                  height: 35.h,
+                ),
+              ],
+            ),
+            CheckoutCard(
+              children: [
+                Text(
+                  "Delivery Method",
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    fontWeight: FontWeight.w500,
+                    height: 1.29.h,
+                    letterSpacing: -0.41.w,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "2 Deliveries \nfrom french & italy",
+                      style: TextStyle(
+                        color: const Color(0xff3c3c43).withOpacity(0.60),
+                        fontSize: 15.sp,
+                        height: 1.33.h,
+                        letterSpacing: -0.24.w,
+                      ),
+                    ),
+                    const Icon(Icons.check),
+                  ],
+                ),
+                Text(
+                  "Free Delivery",
+                  style: TextStyle(
+                      fontSize: 15.sp,
+                      height: 1.33.h,
+                      color: const Color(0xff1C1C1E),
+                      letterSpacing: -0.24.w),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CheckoutCard extends StatelessWidget {
+  final List<Widget> children;
+  const CheckoutCard({
+    Key? key,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      padding: EdgeInsets.only(
+        left: 16.w,
+        top: 16.h,
+        right: 14.w,
+      ),
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ...children,
+          const Divider(
+            thickness: 1,
+            height: 0,
+          ),
+        ],
       ),
     );
   }
